5e51d868c45f2f124a5d4df1f9a4558fb5a81e25
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-12-10 06:41:21 -0400

    3er proyecto HTML y CSS

---
+++ index.js
@@ -0,0 +1,6 @@
+//regex validation
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[0-9]).{6,24}$/;
+const USERNAME_REGEX = /^(?=.*[a-z])(?=.*[0-9]).{6,16}$/;
+const EMAIL_REGEX = /^\S+@\S+\.\S+$/;
+const NUMBER_REGEX = /^[0-9]{6,16}$/;
+
