f74e84ed64ba69b88a745e55569588148f674006
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-12-11 20:31:00 -0400

    finished project

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 //regex validation
-const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[0-9]).{6,24}$/;
 const USERNAME_REGEX = /^(?=.*[a-z])(?=.*[0-9]).{6,16}$/;
 const EMAIL_REGEX = /^\S+@\S+\.\S+$/;
-const NUMBER_REGEX = /^[0-9]{6,16}$/;
+const NUMBER_REGEX = /^[0-9]{10}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[0-9]).{6,24}$/;
 
 //Selectors
 const countries = document.querySelector('#countries');
 const usernameInput = document.querySelector('#username');
 const emailInput = document.querySelector('#email');
 const phoneCode = document.querySelector('#phone-code');
+const phoneInput = document.querySelector('#phone');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirm-password');
+const formBtn = document.querySelector('#form-btn');
+const form = document.querySelector('#form');
+
 
 //Validations
 let usernameValidation = false;
 let emailValidation = false;
+let phoneValidation = false;
+let passwordValidation = false;
+let confirmPasswordValidation = false;
+let countriesValidation = false;
 
 //Function
 
 const validation = (e, validation, element) => {
     const information = e.target.parentElement.children[1];
+    formBtn.disabled = !usernameValidation || !emailValidation || !phoneValidation || !passwordValidation || !confirmPasswordValidation || !countriesValidation ? true : false;
     if (validation) {
         element.classList.add('correct');
         element.classList.remove('incorrect');
@@ -46,8 +57,43 @@ emailInput.addEventListener('input', e => {
 countries.addEventListener('input', e => {
     const optionSelected = [...e.target.children].find(option => option.selected);
     phoneCode.innerHTML = `+${optionSelected.value}`;
+    countriesValidation = optionSelected.value === '' ? false : true;
+    countries.classList.add('correct');
+    phoneCode.classList.add('correct');
+    validation(e, null, null);
 });
 
+phoneInput.addEventListener('input', e => {
+    phoneValidation = NUMBER_REGEX.test(e.target.value);
+    const information = e.target.parentElement.parentElement.children[1];
+    if (phoneValidation) {
+        phoneInput.classList.add('correct');
+        phoneInput.classList.remove('incorrect');
+        information.classList.remove('show-information');
+    } else {
+        phoneInput.classList.add('incorrect');
+        phoneInput.classList.remove('correct');
+        information.classList.add('show-information');
+    }
+});
 
+passwordInput.addEventListener('input', e => {
+    passwordValidation = PASSWORD_REGEX.test(e.target.value);
+    validation(e, passwordValidation, passwordInput);
+});
 
+confirmPasswordInput.addEventListener('input', e => {
+    confirmPasswordValidation = passwordInput.value === e.target.value;
+    validation(e, confirmPasswordValidation, confirmPasswordInput);
+});
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const user = {
+        username: usernameInput.value,
+        email: emailInput.value,
+        phone: `${phoneCode.innerHTML} ${phoneInput.value}`,
+        password: passwordInput.value
+    }
+    console.log(user);
+});
